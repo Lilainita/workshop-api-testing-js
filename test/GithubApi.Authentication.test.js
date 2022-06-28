6905851b4c6153272e1cd2360414043f52b7d779
Liliana <lilainita@gmail.com>
2022-06-28 18:15:19 -0500

    Conflict Fixed

--- test/GithubApi.Authentication.test.js
+++ test/GithubApi.Authentication.test.js
@@ -9,26 +9,26 @@ const repository = 'workshop-api-testing-js';
 require('dotenv').config();
 
 describe('Github Api Test', () => {
-    describe('Authentication', () => {
-        it('Via OAuth2 Tokens by Header', async () => {
-            const response = await axios.get(`${urlBase}/repos/${githubUserName}/${repository}`, {
-                headers: {
-                    Authorization: `token ${process.env.ACCESS_TOKEN}`
-                }
-            });
-            //console.log(response)
-            expect(response.status).to.equal(StatusCodes.OK);
-            expect(response.data.description).equal('This is a Workshop about Api Testing in JavaScript');
-        });
+  describe('Authentication', () => {
+    it('Via OAuth2 Tokens by Header', async () => {
+      const response = await axios.get(`${urlBase}/repos/${githubUserName}/${repository}`, {
+        headers: {
+          Authorization: `token ${process.env.ACCESS_TOKEN}`
+        }
+      });
 
-        it('Via OAuth2 Tokens by parameter', async () => {
-            const response = await axios.get(
-                `${urlBase}/repos/${githubUserName}/${repository}`,
-                { access_token: process.env.ACCESS_TOKEN }
-            );
-
-            expect(response.status).to.equal(StatusCodes.OK);
-            expect(response.data.description).equal('This is a Workshop about Api Testing in JavaScript');
-        });
+      expect(response.status).to.equal(StatusCodes.OK);
+      expect(response.data.description).equal('This is a Workshop about Api Testing in JavaScript');
     });
+
+    it('Via OAuth2 Tokens by parameter', async () => {
+      const response = await axios.get(
+        `${urlBase}/repos/${githubUserName}/${repository}`,
+        { access_token: process.env.ACCESS_TOKEN }
+      );
+
+      expect(response.status).to.equal(StatusCodes.OK);
+      expect(response.data.description).equal('This is a Workshop about Api Testing in JavaScript');
+    });
+  });
 });

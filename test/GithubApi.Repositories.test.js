10ce51718bdcc541a26a46d3957e6f0d519916aa
Liliana <lilainita@gmail.com>
2022-06-30 17:20:05 -0500

    Consume GET Method - global variables added

--- test/GithubApi.Repositories.test.js
+++ test/GithubApi.Repositories.test.js
@@ -10,38 +10,41 @@ const axios = require('axios');
 const urlBase = 'https://api.github.com';
 const repoName = 'jasmine-json-report';
 
+let fullResponse;
+let repo;
+let readmeInfo;
+
 describe('Consuming GET Method GitHub API', () => {
   it('Consume GET Service', async () => {
-    const response = await axios.get(`${urlBase}/users/aperdomob`);
-    expect(response.status).to.equal(StatusCodes.OK);
-    expect(response.data.name).to.eql('Alejandro Perdomo');
-    expect(response.data.company).to.eql('Perficient Latam');
-    expect(response.data.location).to.eql('Colombia');
+    fullResponse = await axios.get(`${urlBase}/users/aperdomob`);
+    expect(fullResponse.status).to.equal(StatusCodes.OK);
+    expect(fullResponse.data.name).to.eql('Alejandro Perdomo');
+    expect(fullResponse.data.company).to.eql('Perficient Latam');
+    expect(fullResponse.data.location).to.eql('Colombia');
   });
 
   it('Repo List Aperdomob', async () => {
-    const repos = await axios.get(`${urlBase}/users/aperdomob/repos`);
-    const repo = repos.data.find((e) => e.name === repoName);
-    expect(repos.data[12].name).to.eql(repo.name);
+    const repos = await axios.get(`${fullResponse.data.repos_url}`);
+    repo = repos.data.find((e) => e.name === repoName);
     expect(repo.full_name).to.eql('aperdomob/jasmine-json-report');
     expect(repo.private).to.eql(false);
     expect(repo.description).to.eql('A Simple Jasmine JSON Report');
   });
 
   it('Downloading jasmine-json-report Repository', async () => {
-    const response = await axios.get(`${urlBase}/repos/aperdomob/${repoName}/zipball/master`);
+    const response = await axios.get(`${repo.url}/zipball/${repo.default_branch}`);
     expect(response.status).to.equal(StatusCodes.OK);
+    expect(response.headers['content-type']).to.equal('application/zip');
   });
 
   it('Checking Name, Path and Sha in README file', async () => {
-    const response = await axios.get(`${urlBase}/repos/aperdomob/${repoName}/contents`);
+    const readmeFile = await axios.get(`${repo.url}/contents`);
     const nameFile = 'README.md';
     const pathFile = 'README.md';
     const shaFile = '360eee6c223cee31e2a59632a2bb9e710a52cdc0';
-
-    const readmeFile = response.data.find((e) => e.name === nameFile);
-    expect(response.status).to.equal(StatusCodes.OK);
-    expect(readmeFile).to.containSubset({
+    readmeInfo = readmeFile.data.find((e) => e.name === nameFile);
+    expect(readmeFile.status).to.equal(StatusCodes.OK);
+    expect(readmeInfo).to.containSubset({
       name: nameFile,
       path: pathFile,
       sha: shaFile
@@ -49,7 +52,7 @@ describe('Consuming GET Method GitHub API', () => {
   });
 
   it('Checking MD5 in README file', async () => {
-    const response = await axios.get('https://raw.githubusercontent.com/aperdomob/jasmine-json-report/master/README.md');
+    const response = await axios.get(`${readmeInfo.download_url}`);
     expect(md5(response)).to.equal('3449c9e5e332f1dbb81505cd739fbf3f');
   });
 });
